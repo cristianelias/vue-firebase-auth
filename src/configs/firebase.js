d7f59049da8a2135ca1b30ffa42d417e8dfe3acc
Cristian Elias <crelias@MacBook-Pro-147.local>
2018-09-12 14:44:41 -0300

    Fix typo.

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,13 +1,12 @@
 import firebase from 'firebase'
 
 firebase.initializeApp({
-  apiKey: process.env.VUE_APP_FIREBASE_VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_VUE_APP_FIREBASE_PROJECT_ID,
-  messagingSenderId:
-    process.env.VUE_APP_FIREBASE_VUE_APP_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
 })
 
 export default firebase
