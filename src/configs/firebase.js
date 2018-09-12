0bd5b6c292d210a1a40a8fe874054e8e4089bb04
Cristian Elias <crelias@MacBook-Pro-147.local>
2018-09-12 01:54:40 -0300

    Revert changes.

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,12 +1,5 @@
 import firebase from 'firebase'
 
-firebase.initializeApp({
-  apiKey: `${process.env.apiKey}`,
-  authDomain: `${process.env.authDomain}`,
-  databaseURL: `${process.env.databaseURL}`,
-  projectId: `${process.env.projectId}`,
-  storageBucket: `${process.env.storageBucket}`,
-  messagingSenderId: `${process.env.messagingSenderId}`,
-})
+firebase.initializeApp(require('./firebase_config.json'))
 
 export default firebase
