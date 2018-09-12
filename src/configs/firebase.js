761ae780ab0963cabca0ddfd90874faf786e1db3
Cristian Elias <crelias@MacBook-Pro-147.local>
2018-09-12 01:39:24 -0300

    Netlify env vars experiment

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,5 +1,12 @@
 import firebase from 'firebase'
 
-firebase.initializeApp(require('./firebase_config.json'))
+firebase.initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+})
 
 export default firebase
