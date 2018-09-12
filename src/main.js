39da75c4c26ce7c3aacc40b1c4ed819358412c4d
Cristian Elias <crelias@MacBook-Pro-147.local>
2018-09-12 13:47:35 -0300

    Add log to understand what netlify is doing.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ Vue.config.productionTip = false
 
 let app
 
+console.log(`variables de entorno: ${JSON.stringify(process.env)}`)
+
 fb.auth().onAuthStateChanged(() => {
   if (!app) {
     new Vue({
